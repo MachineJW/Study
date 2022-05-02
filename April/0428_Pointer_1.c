3dabdc9dfbce65fbf9d7b36bd06da129957ae80a
Jiwoo <98508231+MachineJW@users.noreply.github.com>
2022-05-02 22:36:50 +0900

    Add files via upload

---
+++ April/0428_Pointer_1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main()
+{
+	int a;
+	double b;
+	char c;
+	
+	//주소연산자&
+	// 할당된 메모리 공간의 시작 주소 
+	printf("int 변수 a 의 주소:%x",&a);
+	printf("\ndouble 변수 b 의 주소:%x",&b);
+	printf("\nchar 변수 c 의 주소:%x",&c);
+}
